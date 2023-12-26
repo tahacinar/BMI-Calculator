6cb7ff6c90f3f916b4dde9c4c97d7b7e9aa65ead
Muhammed Taha Çınar <tahacinar@outlook.com.tr>
2023-12-27 00:42:55 +0300

    v1.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.tahacinar.bmicalculator"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.tahacinar.bmicalculator"
