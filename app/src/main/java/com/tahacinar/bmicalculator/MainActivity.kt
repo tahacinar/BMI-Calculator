ba4f7b595fa492154fce2c9271a5cb2949f8f11e
Muhammed Taha Çınar <tahacinar@outlook.com.tr>
2023-12-28 00:09:53 +0300

    v1.0.

--- app/src/main/java/com/tahacinar/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/tahacinar/bmicalculator/MainActivity.kt
@@ -2,11 +2,27 @@ package com.tahacinar.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        val weightText = findViewById<EditText>(R.id.etWeight)
+        val heightText = findViewById<EditText>(R.id.etHeight)
+        val calcButton = findViewById<Button>(R.id.btnCalculate)
+
+        calcButton.setOnClickListener {
+            val weight = weightText.text.toString()
+            val height = heightText.text.toString()
+
+            val bmi = weight.toFloat() / ((height.toFloat() / 100) * (height.toFloat() / 100))
+            // get result with two decimal places
+            val bmi2Digits = String.format("%.2f", bmi).toFloat()
+
+
+        }
+    }
 }
